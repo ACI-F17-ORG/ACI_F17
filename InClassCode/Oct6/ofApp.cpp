a6b5717560a83e0172af595b0a34406e8d6d2fe8
Shawn Lawson <lawsos2@rpi.edu>
2017-10-07 09:00:07 -0400

    adding oct 6 code

---
+++ InClassCode/Oct6/ofApp.cpp
@@ -0,0 +1,97 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    camWidth = 320;
+    camHeight = 240;
+    column = 0;
+    
+    vidGrabber.setVerbose(true);
+    vidGrabber.setDeviceID(0);
+    vidGrabber.setDesiredFrameRate(30);
+    vidGrabber.initGrabber(camWidth, camHeight);
+    
+    vidPixels.allocate(camWidth, camHeight, OF_PIXELS_RGB);
+    vidTexture.allocate(vidPixels);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    vidGrabber.update();
+
+//    if(vidGrabber.isFrameNew())
+    {
+        ofPixelsRef pixelRef = vidGrabber.getPixels();
+        column = (column +1) % camWidth;
+        for (int i = 0; i < camHeight; ++i) {
+            vidPixels.setColor(column,
+                               i,
+                               pixelRef.getColor(column, i));
+        }
+        vidTexture.loadData(vidPixels);
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetColor(ofColor::white);
+    vidGrabber.draw(20, 20);
+    
+    vidTexture.draw(camWidth + 40, 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ InClassCode/Oct6/readme.md
@@ -0,0 +1,3 @@
+Free Scannin
+
+https://www.youtube.com/watch?v=1lWJXDG2i0A
